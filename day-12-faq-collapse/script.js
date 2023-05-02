642b7ef1a7f02e20cfd7fabeeec541f8d5b6f127
Muhammad Ali <791600ali@gmail.com>
2023-05-02 19:29:58 +1000

    donm

--- day-12-faq-collapse/script.js
+++ day-12-faq-collapse/script.js
@@ -1,7 +1,17 @@
 const btns = document.querySelectorAll('.btn');
+const faClose = document.querySelectorAll('.fa-close');
+const btna = document.querySelectorAll('.btna');
 
 btns.forEach(btn => {
   btn.addEventListener('click', () => {
-  btn.parentNode.classList.toggle('active');
+  btn.parentNode.classList.add('active');
+  btn.parentNode.innerHTML =   `${btn.parentNode.innerHTML} <button class="btna"><i class="fas fa-close"></i></button>`;
+  btn.classList.add('after');
+  })
+})
+
+btna.forEach(btn => {
+  btn.addEventListener('click', () => {
+    btn.parentNode.classList.remove('active');
   })
 })
