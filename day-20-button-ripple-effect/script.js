eeefebe9d24e0db0db7e4e61a8f622e580cbcb93
Muhammad Ali <791600ali@gmail.com>
2023-05-10 15:59:57 +1000

    butn

---
+++ day-20-button-ripple-effect/script.js
@@ -0,0 +1,15 @@
+const button = document.querySelector('.btn');
+
+
+button.addEventListener('click', (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+
+  const buttonTop = e.target.offsetTop;
+  const buttonLeft = e.target.offsetLeft;
+  const trueX = x - buttonLeft;
+  const trueY = y - buttonTop;
+
+  console.log(trueX, trueY);
+  button.innerHTML = `Click Me   <span class="circle" style="top: ${trueY}px; left: ${trueX}px;"></span>`
+})
