8df99d4f9e4eaa3baeb4b9705ef40b4d99b0461d
Muhammad Ali <791600ali@gmail.com>
2023-05-12 15:05:10 +1000

    pt 1

---
+++ day-22-hover-board/script.js
@@ -0,0 +1,45 @@
+const boxes = document.querySelectorAll('.box');
+const colors = ['#FFD1B8', '#FFECB8', '#F4FFB8', '#B8FFD8', '#B8EFFF', '#D1B8FF'];
+const nobBtn = document.querySelectorAll('.n');
+const boxesDiv = document.querySelector('.boxes');
+
+function colorfr(numberForTimeout) {
+  boxes.forEach(box => {
+    box.addEventListener('mouseover', function() {
+      pickRandomColor();
+      box.style.backgroundColor = pickRandomColor();
+      setTimeout(() => {
+      box.style.backgroundColor = "rgb(108, 108, 108)";
+      }, numberForTimeout);
+    })
+  });
+}
+
+colorfr(7000)
+
+
+function pickRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+nobBtn.forEach(btn => {
+  btn.addEventListener('click', function(){
+    nobBtn.forEach(btn => { btn.classList.remove('active')})
+    btn.classList.add('active')
+    const numberForTimeout = +btn.getAttribute('data-number');
+    console.log(numberForTimeout)
+    colorfr(numberForTimeout)
+  })
+})
+
+// function createBox(numberOfBoxes) {
+//   const box = document.createElement('div')
+//   box.classList.add('box')
+//   boxesDiv.appendChild(box)
+// }
+
+// function repeatFunctionByN(func, n) {
+//   for (let i = 0; i < n; i++) {
+//     func();
+//   }
+// }
